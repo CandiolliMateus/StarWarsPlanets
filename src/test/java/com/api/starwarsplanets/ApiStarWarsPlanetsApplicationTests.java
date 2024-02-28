4287691301631e9e5f92d2c37eeb12b63a35fa4b
CandiolliMateus <mateuscandiolli.1909@gmail.com>
2024-02-28 20:53:15 -0300

    iniciando api

---
+++ src/test/java/com/api/starwarsplanets/ApiStarWarsPlanetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.starwarsplanets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStarWarsPlanetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
